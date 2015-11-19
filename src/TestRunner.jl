2618ad53cb9be4065a01c6261d4980c3dd8e96ae
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2015-11-19 05:26:06 +0100

    Added draft of new approach to test structure parsing

--- src/TestRunner.jl
+++ src/TestRunner.jl
@@ -1,6 +1,34 @@
 module TestRunner
+export TestStructureNode,FactsNode,ContextNode,FactNode
 
-get_file_content(testFilePath::AbstractString) = testFilePath |> readall |> parse
+abstract TestStructureNode
+
+type FactsNode <: TestStructureNode
+    line::Int
+    name::AbstractString
+    childs::Vector{TestStructureNode}
+end
+
+type ContextNode <: TestStructureNode
+    line::Int
+    name::AbstractString
+    childs::Vector{TestStructureNode}
+end
+
+type FactNode <: TestStructureNode
+    line::Int
+    name::AbstractString
+    result::Nullable{Bool}
+    details::Nullable{AbstractString}
+end
+FactNode(line::Int, name::AbstractString) = FactNode(line,name,Nullable{Bool}(),Nullable{AbstractString}())
+FactNode(line::Int, name::AbstractString, result::Nullable{Bool}) = FactNode(line, name, result, Nullable{AbstractString}())
+
+
+function get_file_content(testFilePath::AbstractString)
+      content = testFilePath |> readall
+      "begin\n"*content*"\nend" |> parse
+end
 expand_expression(e::Expr) = map((arg -> typeof(arg) == Expr? expand_expression(arg):arg), e.args)
 
 function find_subparts(tree::Array, a::Symbol, results::Array = [])
@@ -26,5 +54,32 @@ function get_facts_groups(testFileContent::Expr)
   facts_groups = find_subparts(expanded_expression, :facts)
   map( group -> (get_message(group), get_facts(group)) ,facts_groups)
 end
+
+function f(ex::Expr, line = 0)
+    results = Vector{TestStructureNode}()
+    for e in ex.args
+        if isa(e, LineNumberNode)
+            line = e.line
+        elseif isa(e,Expr)
+            if length(e.args)>0 && e.args[1] == :facts
+                childs = f(e.args[2],line)
+                node = FactsNode(line, length(e.args)>2?e.args[3]:"",childs)
+                push!(results, node)
+            elseif length(e.args)>0 && e.args[1] == :context
+                childs = f(e.args[2],line)
+                node = ContextNode(line, length(e.args)>2?e.args[3]:"",childs)
+                push!(results, node)
+            elseif length(e.args)>0 && e.args[1] == Symbol("@fact")
+               node = FactNode(line, length(e.args)>2?e.args[3]:"")
+               push!(results, node)
+            else
+                append!(results, f(e, line))
+            end
+        end
+    end
+    return results
+end
+get_tests_structure(testFileContent::Expr) =  testFileContent |> f
+
 
 end # module
